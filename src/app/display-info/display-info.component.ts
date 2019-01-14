8dc0c4bbd85ab553968810dfbef348a4a31c906f
thomas <thomas.martin6070@gmail.com>
2019-01-14 15:01:43 +0100

    clean

--- src/app/display-info/display-info.component.ts
+++ src/app/display-info/display-info.component.ts
@@ -7,33 +7,10 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./display-info.component.css']
 })
 export class DisplayInfoComponent implements OnInit {
-  @Input()user:object;
-  todoList: object;
-  isLoaded = false;
-  constructor(private http: HttpClient) {
+
+  constructor() {
  }
-  configUrl = 'https://jsonplaceholder.typicode.com/todos/1';
   ngOnInit() {
-    this.http.get(this.configUrl)
-      .subscribe(
-        value => {
-          this.todoList = value;
-          this.isLoaded = true;
-        },
-        error => {
-          console.log(error);
-        }
-      );
-  }
-  tab = ['data1','data2','data3','data4','data5'];
 
-  onClickMe($event){
-    console.log($event);
   }
-
-  playlists=['playlist 1','playlist 2','playlist 3','playlist 4']
-  addPlaylist(playlist: string){
-    this.playlists.push(playlist);
-  }
-
 }
