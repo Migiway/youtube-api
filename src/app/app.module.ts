ef9b7bd647202f625b06a08c1bc031d565a1e616
thomas <thomas.martin6070@gmail.com>
2019-02-04 08:39:20 +0100

    front angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,22 @@ import { DisplayInfoComponent } from './display-info/display-info.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TendancesComponent } from './tendances/tendances.component';
+import { MusiqueComponent } from './musique/musique.component';
+import { SportComponent } from './sport/sport.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DisplayInfoComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    TendancesComponent,
+    MusiqueComponent,
+    SportComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
